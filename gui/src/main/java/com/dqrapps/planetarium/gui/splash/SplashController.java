4a292c9603e5446d160e166be8010bf16239db57
dqromney <iag15501>
2021-06-03 17:25:46 -0600

    Playing with Setup Menu.

--- gui/src/main/java/com/dqrapps/planetarium/gui/splash/SplashController.java
+++ gui/src/main/java/com/dqrapps/planetarium/gui/splash/SplashController.java
@@ -2,13 +2,25 @@ package com.dqrapps.planetarium.gui.splash;
 
 import com.dqrapps.planetarium.gui.App;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SplashController {
+public class SplashController implements Initializable {
+
+    @FXML
+    private StackPane rootPane;
 
     @FXML
     private void switchToConfig() throws IOException {
         App.setRoot("config");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
     }
 }
