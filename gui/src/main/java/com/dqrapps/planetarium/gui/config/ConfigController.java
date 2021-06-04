ca0eb1b16cfaeac6a9efb5fffdcd778c4ba01239
David Romney <dromney@incomm.com>
2021-06-04 00:51:05 -0600

    Updated config.fxml using grid component.

--- gui/src/main/java/com/dqrapps/planetarium/gui/config/ConfigController.java
+++ gui/src/main/java/com/dqrapps/planetarium/gui/config/ConfigController.java
@@ -1,11 +1,26 @@
 package com.dqrapps.planetarium.gui.config;
 
 import com.dqrapps.planetarium.gui.App;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
 
 public class ConfigController {
+
+    ObservableList<String> plotModeList = FXCollections
+            .observableArrayList("North", "South");
+
+    @FXML
+    private ComboBox plotModeBox;
+
+    @FXML
+    private void initialize() {
+        plotModeBox.setValue("North");
+        plotModeBox.setItems(plotModeList);
+    }
 
     @FXML
     private void switchToPlot() throws IOException {
