24ffb3e393a6a40e196fbc5891f6f1d94de83299
David Romney <dromney@incomm.com>
2021-06-04 01:03:58 -0600

    Adding horizon and fix plotMode in UI.

--- gui/src/main/java/com/dqrapps/planetarium/gui/config/ConfigController.java
+++ gui/src/main/java/com/dqrapps/planetarium/gui/config/ConfigController.java
@@ -1,6 +1,8 @@
 package com.dqrapps.planetarium.gui.config;
 
 import com.dqrapps.planetarium.gui.App;
+import com.dqrapps.planetarium.logic.type.Horizon;
+import com.dqrapps.planetarium.logic.type.PlotMode;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,15 +12,21 @@ import java.io.IOException;
 
 public class ConfigController {
 
+    ObservableList<String> horizonList = FXCollections
+            .observableArrayList(Horizon.NORTH.getToken(), Horizon.SOUTH.getToken());
     ObservableList<String> plotModeList = FXCollections
-            .observableArrayList("North", "South");
+            .observableArrayList(PlotMode.INDIVIDUAL.getMode(), PlotMode.CONTINUOUS.getMode());
 
+    @FXML
+    private ComboBox horizonBox;
     @FXML
     private ComboBox plotModeBox;
 
     @FXML
     private void initialize() {
-        plotModeBox.setValue("North");
+        horizonBox.setValue(Horizon.NORTH.getToken());
+        horizonBox.setItems(horizonList);
+        plotModeBox.setValue(PlotMode.INDIVIDUAL.getMode());
         plotModeBox.setItems(plotModeList);
     }
 
