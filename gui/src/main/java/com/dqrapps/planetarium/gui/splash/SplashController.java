9cae72eb6039578c590bc57be25a6198ed594d67
dqromney <iag15501>
2021-06-07 08:19:17 -0600

    Playing with Setup Menu.

--- gui/src/main/java/com/dqrapps/planetarium/gui/splash/SplashController.java
+++ gui/src/main/java/com/dqrapps/planetarium/gui/splash/SplashController.java
@@ -8,7 +8,8 @@ import javafx.scene.layout.StackPane;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
+// JavaFX Splash Screen video
+// https://www.genuinecoder.com/javafx-splash-screen-loading-screen/#:~:text=JavaFX%20Splash%20Screen%20Splash%20screens%20are%20awesome.%20They,JavaFX%20Splash%20Screen%20%2F%20Loading%20Screen%20with%20animation.
 public class SplashController implements Initializable {
 
     @FXML
